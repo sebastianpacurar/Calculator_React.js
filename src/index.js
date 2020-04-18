05f733521d285d4701e199fddb9204d512b12468
me <pacurar.sebastian90@gmail.com>
2020-04-18 22:30:55 +0300

    The following changes have been made:   - added PI, factorial, open close braces   - created a separate method for adding minus operator   - improved functionality for calculating complex mathematics (for trigonometric functions)

--- src/index.js
+++ src/index.js
@@ -67,6 +67,16 @@ class Calculator extends React.Component {
         };
     }
 
+    countElements = element => {
+        let count = 0;
+        for (let index = 0; index < this.state.input.length; index++) {
+            if (this.state.input.charAt(index) === element) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     // last element of the input
     lastInputElement = () => {
         return this.state.input[this.state.input.length - 1];
@@ -80,8 +90,8 @@ class Calculator extends React.Component {
     // add value to input
     addNumber = val => {
 
-        // if input ends with ")" do not add number
-        if (!this.state.input.endsWith(")")) {
+        // if input ends with ")" and contains a symbol do not add number
+        if (!this.state.input.endsWith(")") && !this.state.input.match(/[a-z!π]/)) {
             // append the number to input
             this.setState({
                 input: this.state.input + val,
@@ -106,48 +116,113 @@ class Calculator extends React.Component {
         }
     };
 
+    // add open parentheses
+    addOpenParentheses = val => {
+
+        // if input ends with open parentheses and number of ")" is equal to numbber of "("
+        if (!this.state.input.endsWith("(") && (this.countElements(")") === this.countElements("("))) {
+
+            // if it doesn't end with a Number and the last element is not ")" and there are letters and symbols do nothing
+            if (isNaN(Number(this.lastInputElement())) && !this.state.input.endsWith(")") && !this.state.input.match(/[a-z!π]/)) {
+                this.setState({
+                    input: this.state.input + val,
+                    number: this.state.input + val
+                });
+            }
+
+        }
+    };
+
+    // add close parentheses
+    addCloseParentheses = val => {
+
+        // if input last element is a number, and number of "(" is greater than the number of ")"
+        if (!isNaN(Number(this.lastInputElement())) && (this.countElements("(") > this.countElements(")"))) {
+
+            this.setState({
+                input: this.state.input + val,
+                number: this.state.input + val
+            });
+        }
+    };
+
+    addMinusOperator = val => {
+        // add negative negative number in case it's first number and when input is empty,
+        //   or it ends with "(", "/", or "*"
+        if (val === "-" && (this.state.input === "" ||
+            this.state.input.endsWith("(") || this.state.input.endsWith("/") ||
+            this.state.input.endsWith("*")) && !this.state.input.match(/[a-z!π]/)) {
+
+            this.setState({
+                input: this.state.input + val,
+                number: this.state.input + val
+            });
+
+        }
+
+        // if last element is a number, and there is no trigonometry, factorial or pi add minus
+        else if ((!isNaN(Number(this.lastInputElement())) || this.lastInputElement() === ")") &&
+            !this.state.input.match(/[a-z!π]/)) {
+            this.setState({
+                input: this.state.input + val,
+                number: this.state.input + val
+            });
+        }
+    }
+
     // add simple operators
     addOperator = val => {
 
-        // add negative "-" for first input number
-        if (val === "-" && this.state.input === "") {
-            this.setState({input: "-"});
-        }
-
-        // prevent adding operator if last element is not a number
-        else if (!isNaN(Number(this.lastInputElement()))) {
+        // prevent adding operator if last element is not a number or if it doesn't end with a ")"
+        if (!isNaN(Number(this.lastInputElement())) || this.lastInputElement() === ")") {
             switch (val) {
                 case "sin(x)":
-                    // avoid calling sin multiple times on the same input
-                    if (!this.state.input.includes("sin")) {
+                    // avoid calling sin multiple times on the same input and if no. of "(" equals to no. of ")"
+                    if (!this.state.input.match(/[a-z!π]/) && this.countElements("(") === this.countElements(")")) {
                         this.setState({input: `sin(${this.state.input})`});
                     }
                     break;
 
                 case "cos(x)":
-                    // avoid calling cos multiple times on the same input
-                    if (!this.state.input.includes("cos")) {
+                    // avoid calling cos multiple times on the same input and if no. of "(" not equals no. of ")"
+                    if (!this.state.input.match(/[a-z!π]/) && this.countElements("(") === this.countElements(")")) {
                         this.setState({input: `cos(${this.state.input})`});
                     }
                     break;
 
                 case "tan(x)":
-                    // avoid calling tan multiple times on the same input
-                    if (!this.state.input.includes("tan")) {
+                    // avoid calling tan multiple times on the same input and if no. of "(" not equals no. of ")"
+                    if (!this.state.input.match(/[a-z!π]/) && this.countElements("(") === this.countElements(")")) {
                         this.setState({input: `tan(${this.state.input})`});
                     }
                     break;
 
                 case "cot(x)":
-                    // avoid calling cot multiple times on the same input
-                    if (!this.state.input.includes("cot")) {
+                    // avoid calling cot multiple times on the same input and if no. of "(" not equals no. of ")"
+                    if (!this.state.input.match(/[a-z!π]/) && this.countElements("(") === this.countElements(")")) {
                         this.setState({input: `cot(${this.state.input})`});
+                    }
+                    break;
+
+                case "!":
+                    // avoid calling tan multiple times on the same input and if no. of "(" not equals no. of ")"
+                    if (!this.state.input.match(/[a-z!π]/) && this.countElements("(") === this.countElements(")")) {
+                        this.setState({input: `(${this.state.input})!`});
+                    }
+                    break;
+
+                case "π":
+                    // avoid calling tan multiple times on the same input and if no. of "(" not equals no. of ")"
+                    if (!this.state.input.match(/[a-z!π]/) && this.countElements("(") === this.countElements(")")) {
+                        this.setState({input: `(${this.state.input})π`});
                     }
                     break;
 
                 // for simple operators
                 default:
-                    if (this.lastInputElement() !== val) {
+
+                    // if same operator is not clicked twice, and input does not contains letters
+                    if (this.lastInputElement() !== val && !this.state.input.match(/[a-z!π]/)) {
                         this.setState({
                             input: this.state.input + val,
                             number: this.state.number + val
@@ -181,8 +256,9 @@ class Calculator extends React.Component {
         // if input is empty, do nothing
         if (this.state.input.length > 0) {
 
-            // if last element is not a number or does not end with ")" do not calculate
-            if (!isNaN(Number(this.lastInputElement())) || this.state.input.endsWith(")")) {
+            // if last element is a number or ")", or "!", or "π" calculate
+            if (!isNaN(Number(this.lastInputElement())) || this.state.input.endsWith(")")
+                || this.state.input.endsWith("!") || this.state.input.endsWith("π")) {
 
                 let result = this.state.input;        // save the input state into result variable
                 this.setState({input: ""});     // reset the input state to empty string
@@ -231,6 +307,49 @@ class Calculator extends React.Component {
                     });
                 }
 
+                // calculate factorial
+                else if (result.endsWith("!")) {
+
+                    // the factorial will be parsed between braces therefore operations will be evaluated before factorial
+                    let number = Number(eval(this.state.number));
+
+                    // the limit for factorial is 170. Otherwise it outputs "Infinity".
+                    // To avoid RangeError: Maximum call stack size exceeded, set the limit to 170 factorial
+                    if (number > 170) {
+                        this.setState({output: "Number is too large"});
+                        return;
+                    }
+
+                    // if number is negative throw Negative Factorial Error
+                    else if (number < 0) {
+                        this.setState({output: "Negative Factorial Error"});
+                        return;
+                    }
+
+                    // calculate factorial for the given number
+                    const factorial = num => {
+                        if (num === 0) {
+                            return 1;
+                        } else {
+                            return num * factorial(num - 1);
+                        }
+                    };
+
+                    const finalResult = factorial(number);      // the result from the factorial function
+                    this.setState({
+                        output: finalResult,
+                        number: ""
+                    });
+                }
+
+                // calculate PI
+                else if (result.includes("π")) {
+                    this.setState({
+                        output: eval(this.state.number) * Math.PI,
+                        number: ""
+                    });
+                }
+
                 // for all common operators such as addition, subtraction, multiplication division and exponential
                 else {
                     result = eval(result);
@@ -255,14 +374,12 @@ class Calculator extends React.Component {
                     <Output>{this.state.output}</Output>
                     <div className={'button-row'}>
                         <Button
-                            type={'button'}
                             className={'special-button'}
                             value={'Radians'}
                             label={'Radians'}
                             onClick={this.isDegreeOn}
                             disabled={!this.state.degree}/>
                         <Button
-                            type={'button'}
                             className={'special-button'}
                             value={'Degrees'}
                             label={'Degrees'}
@@ -305,7 +422,7 @@ class Calculator extends React.Component {
                             className={'operator'}
                             value={'-'}
                             label={'-'}
-                            onClick={this.addOperator}/>
+                            onClick={this.addMinusOperator}/>
                     </div>
                     <div className={"button-row"}>
                         <Button
@@ -367,6 +484,28 @@ class Calculator extends React.Component {
                             value={'cot(x)'}
                             label={'cot(x)'}
                             onClick={this.addOperator}/>
+                    </div>
+                    <div className={"button-row"}>
+                        <Button
+                            className={'operator'}
+                            value={'!'}
+                            label={'X!'}
+                            onClick={this.addOperator}/>
+                        <Button
+                            className={'operator'}
+                            value={'π'}
+                            label={'π'}
+                            onClick={this.addOperator}/>
+                        <Button
+                            className={'operator'}
+                            value={'('}
+                            label={'('}
+                            onClick={this.addOpenParentheses}/>
+                        <Button
+                            className={'operator'}
+                            value={')'}
+                            label={')'}
+                            onClick={this.addCloseParentheses}/>
                     </div>
                     <div className={'button-row'}>
                         <Button
